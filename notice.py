76953c979cc32db12ae2cb0a3b0c994727455489
root <root@ip-172-31-12-164.ap-northeast-2.compute.internal>
2021-11-04 13:44:50 +0000

    selenium setup

--- notice.py
+++ notice.py
@@ -1,1 +1,14 @@
 from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# driver = webdriver.Chrome("/usr/bin/chromedriver")
+driver = webdriver.Chrome(options=options)
+url = 'https://www.mju.ac.kr/mjukr/257/subview.do'
+
+driver.get(url)
+
+print(driver.title)
