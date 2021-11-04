6f7f913057b01292551a392b8acf1bcd1ef5b656
hou27 <ataj125@gmail.com>
2021-11-04 14:18:28 +0000

    scrapping titles

--- notice.py
+++ notice.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -7,8 +9,21 @@ options.add_argument("--disable-dev-shm-usage")
 
 # driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver = webdriver.Chrome(options=options)
-url = 'https://www.mju.ac.kr/mjukr/257/subview.do'
+# url = 'https://www.mju.ac.kr/mjukr/257/subview.do'
+url = 'https://www.mju.ac.kr/mjukr/257/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGbWp1a3IlMkYxNDMlMkZhcnRjbExpc3QuZG8lM0ZiYnNDbFNlcSUzRCUyNmJic09wZW5XcmRTZXElM0QlMjZpc1ZpZXdNaW5lJTNEZmFsc2UlMjZpc1ZpZXclM0R0cnVlJTI2c3JjaENvbHVtbiUzRHNqJTI2c3JjaFdyZCUzRCVFQyVBMCU4NCVFQSVCMyVCQyUyNg%3D%3D'
 
 driver.get(url)
 
 print(driver.title)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+html = soup.find("table", {"class": "artclTable artclHorNum1"})
+
+notice_headline = html.find('tbody').find_all("tr")
+
+for notice in notice_headline:
+    title = notice.find('td', {'class': '_artclTdTitle'}).find('a').find('strong').string
+    print(title)
+    
+driver.quit()
